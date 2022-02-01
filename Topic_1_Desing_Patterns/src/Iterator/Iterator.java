78a1f3eb83c28c3bf6d5975f8f4f8146d986cfc3
Mateo Tavera Velez <mateo.tavera@globant.com>
2022-02-01 16:22:03 -0500

    Practice Code 1

---
+++ Topic_1_Desing_Patterns/src/Iterator/Iterator.java
@@ -0,0 +1,8 @@
+package Iterator;
+
+public interface Iterator {
+
+    boolean hasNext();//Condition
+    String current();//Action
+    void next();//Counter
+}
---
+++ Topic_1_Desing_Patterns/src/Iterator/Main.java
@@ -0,0 +1,24 @@
+package Iterator;
+
+/** USed to acces to he container's elements
+ * History. "Go back button"
+ */
+public class Main {
+    public static void main(String[] args) {
+
+        BrowseHistory history = new BrowseHistory();
+        history.push("a");
+        history.push("b");
+        history.push("c");
+
+        Iterator iterator = history.createIterator();
+        while (iterator.hasNext()){
+            String url = iterator.current();
+            System.out.println(url);
+            iterator.next();
+
+        }
+    }
+
+
+}
