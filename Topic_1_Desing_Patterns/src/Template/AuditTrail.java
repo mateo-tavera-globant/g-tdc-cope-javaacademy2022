0418e03c2e0301e63bfac238f0c08ef761c212bc
Mateo Tavera Velez <mateo.tavera@globant.com>
2022-02-01 20:09:33 -0500

    Practice Code 1

---
+++ Topic_1_Desing_Patterns/src/Template/AuditTrail.java
@@ -0,0 +1,8 @@
+package Template;
+
+public class AuditTrail {
+
+    public void record(){
+        System.out.println("Audit");
+    }
+}
--- Topic_1_Desing_Patterns/src/Template/Main.java
+++ Topic_1_Desing_Patterns/src/Template/Main.java
@@ -1,4 +1,13 @@
 package Template;
 
+/** Create a general skeleton for similar functionalities
+ *
+ */
+
 public class Main {
+    public static void main(String[] args) {
+
+        TransferMoneyTask task = new TransferMoneyTask();
+        task.execute();
+    }
 }
