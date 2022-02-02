2b3e6b152abf05e7b43b564d1241086f311c6c74
Mateo Tavera Velez <mateo.tavera@globant.com>
2022-02-01 20:27:39 -0500

    Practice Code 1

--- Topic_1_Desing_Patterns/src/Command/Main.java
+++ Topic_1_Desing_Patterns/src/Command/Main.java
@@ -1,4 +1,16 @@
 package Command;
 
+/**
+ * Delegate work to a receiver
+ */
+
 public class Main {
+    public static void main(String[] args) {
+
+        CustomerService service = new CustomerService();
+        Command command = new AddCustomerCommand(service);
+        Button button = new Button(command);
+        button.click();
+    }
+
 }
