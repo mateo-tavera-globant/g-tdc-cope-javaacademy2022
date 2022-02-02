37e1cab893d6f4f2891b42a629c5b208b88b49cb
Mateo Tavera Velez <mateo.tavera@globant.com>
2022-02-01 23:23:40 -0500

    Practice Code 1

---
+++ Topic_1_Desing_Patterns/src/Observer/Chart.java
@@ -0,0 +1,17 @@
+package Observer;
+
+public class Chart implements Observer {
+
+    private DataSource dataSource;
+
+    public Chart(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+
+    @Override
+    public void update() {
+        System.out.println("Chart got updated: " + dataSource.getValue());
+    }
+
+}
--- Topic_1_Desing_Patterns/src/Observer/Main.java
+++ Topic_1_Desing_Patterns/src/Observer/Main.java
@@ -6,6 +6,17 @@ package Observer;
 public class Main {
     public static void main(String[] args) {
 
+        DataSource dataSource = new DataSource();
+        SpreadSheet sheet1 = new SpreadSheet(dataSource);
+        SpreadSheet sheet2 = new SpreadSheet(dataSource);
+        Chart chart = new Chart(dataSource);
+
+        dataSource.addObserver(sheet1);
+        dataSource.addObserver(sheet2);
+        dataSource.addObserver(chart);
+
+        dataSource.setValue(1);
+
 
     }
 }
