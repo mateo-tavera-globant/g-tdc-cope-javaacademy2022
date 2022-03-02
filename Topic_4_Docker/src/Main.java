c6c6bdeaf644d1a4671e5268101b25d8ed448c30
Mateo Tavera Velez <mateo.tavera@globant.com>
2022-03-01 23:23:59 -0500

    Practice Code 4

---
+++ Topic_4_Docker/src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Creat a simple java program and running
+ * whitin a Docker container
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
