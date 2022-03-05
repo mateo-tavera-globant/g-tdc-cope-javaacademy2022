695ea9a53e9f0b6a41aabfe225eb72a9e75c55af
Mateo Tavera Velez <mateo.tavera@globant.com>
2022-03-05 14:44:40 -0500

    Practice code 4

--- Topic_4_Docker/src/Main.java
+++ Topic_4_Docker/src/Main.java
@@ -7,4 +7,22 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+
+    /*
+    DOCKER:
+
+    A container is an instance of an image. Docker runs the container
+    from an image, which have the OS (only distribution, because kernel is shared
+    with the host), the software (Java) and the app (myCode)
+    If we want to create a container, we run an image. How?
+        docker run *image form docker hub*:*version I want*
+        If I don't specify the taf or version, it downloads the latest
+
+        docker pull: downloads the image
+        docker run: downloads and runs the image
+
+
+
+
+     */
 }
